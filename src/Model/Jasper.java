d281a991bedb3db278ad6902ba26cebbe5bac630
Lochana Pahansilu <123851797+lochana2005@users.noreply.github.com>
2024-12-24 20:19:10 +0530

    Update Jasper.java

--- src/Model/Jasper.java
+++ src/Model/Jasper.java
@@ -13,22 +13,14 @@ import java.sql.Connection;
 /**
  *
  * Create a Object of this class and call viewReport Method
- * <li>Author Buddhi7770</li>
+ * <li>Author Lochana2005</li>
  * <li>Copyright 2024</li>
  *
- * @author Buddhi7770
+ * @author Lochana2005
  */
 public class Jasper {
 
-    /**
-     *
-     * @param name Name of the jasperReport Created in Reports Folder in your
-     * project
-     * @param parameters HashMap of Parameters
-     * @param table Table of DataSource
-     * @return jasperPrint
-     * @throws java.lang.Exception
-     */
+  
     public JasperPrint viewReport(String name, HashMap parameters, JTable table) throws Exception {
         this.rootPath += name;
         JRTableModelDataSource dataSource = new JRTableModelDataSource(table.getModel());
@@ -37,15 +29,7 @@ public class Jasper {
         return jasperPrint;
     }
 
-    /**
-     *
-     * @param name Name of the jasperReport Created in Reports Folder in your
-     * project
-     * @param parameters HashMap of Parameters
-     * @param connection MySQL Database connection
-     * @return jasperPrint
-     * @throws java.lang.Exception
-     */
+   
     public JasperPrint viewReport(String name, HashMap parameters, Connection connection) throws Exception {
         this.rootPath += name;
         JasperPrint jasperPrint = JasperFillManager.fillReport(Jasper.class.getResourceAsStream(this.rootPath), parameters, connection);
@@ -53,16 +37,7 @@ public class Jasper {
         return jasperPrint;
     }
 
-    /**
-     *
-     * A EmptyDatasource will be generated
-     *
-     * @param name Name of the jasperReport Created in Reports Folder in your
-     * project
-     * @param parameters HashMap of Parameters
-     * @return jasperPrint
-     * @throws java.lang.Exception
-     */
+   
     public JasperPrint viewReport(String name, HashMap parameters) throws Exception {
         this.rootPath += name;
         JREmptyDataSource dataSource = new JREmptyDataSource();
@@ -71,14 +46,7 @@ public class Jasper {
         return jasperPrint;
     }
 
-    /**
-     * A Empty HashMap and EmptyDataSoucre will be generated
-     *
-     * @param name Name of the jasperReport Created in Reports Folder in your
-     * project
-     * @return jasperPrint
-     * @throws java.lang.Exception
-     */
+   
     public JasperPrint viewReport(String name) throws Exception {
         this.rootPath += name;
         HashMap parameters = new HashMap();
@@ -88,24 +56,12 @@ public class Jasper {
         return jasperPrint;
     }
 
-    /**
-     * A JasperPrint returned by viewReport
-     *
-     * @param jasperPrint JasperPrint to Export as PDF
-     * @param fileName FileName of the PDF
-     * @throws java.lang.Exception
-     */
+    
     public void exportReportPDF(JasperPrint jasperPrint, String fileName) throws Exception {
         JasperExportManager.exportReportToPdfFile(jasperPrint, fileName);
     }
 
-    /**
-     * A JasperPrint returned by viewReport
-     *
-     * @param jasperPrint JasperPrint to Export as HTML
-     * @param fileName Filename of the HTML
-     * @throws java.lang.Exception
-     */
+   
     public void exportReportHTML(JasperPrint jasperPrint, String fileName) throws Exception {
         JasperExportManager.exportReportToHtmlFile(jasperPrint, fileName);
     }
